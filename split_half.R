f8b966462e6672a5808b3d36a6aac2d4269f6573
Liang Zhang <psychelzh@outlook.com>
2021-12-21 09:29:56 +0800

    enhance code quality
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- split_half.R
+++ split_half.R
@@ -6,16 +6,21 @@ box::use(
 )
 index_map <- readr::read_csv("config/index_map.csv", show_col_types = FALSE) |>
   tibble::deframe()
-c("AntiSac", "CateSwitch", "ShiftColor", "ShiftNumber", "spatialWM", "Stroop", "WM3") |>
-  tibble::as_tibble_col(column_name = "task") |>
-  expand_grid(half = c("Even", "Odd")) |>
+expand_grid(
+  task = names(index_map),
+  half = c("Even", "Odd")
+) |>
+  mutate(
+    file_data = map2(
+      task, half,
+      ~ fs::dir_ls("EFRes", regexp = str_c(.x, ".+", .y), ignore.case = TRUE)
+    )
+  ) |>
+  unnest(file_data) |>
   mutate(
     indices = map2(
-      task, half,
-      ~ readr::read_csv(
-        fs::dir_ls("EFRes", regexp = str_c(.x, ".+", .y), ignore.case = TRUE),
-        show_col_types = FALSE
-      ) |>
+      task, file_data,
+      ~ readr::read_csv(.y, show_col_types = FALSE) |>
         inner_join(
           readxl::read_excel(
             fs::dir_ls("config", regexp = .x, ignore.case = TRUE)
@@ -29,7 +34,7 @@ c("AntiSac", "CateSwitch", "ShiftColor", "ShiftNumber", "spatialWM", "Stroop", "
     )
   ) |>
   unnest(indices) |>
-  pivot_wider(names_from = "half", values_from = "index") |>
+  pivot_wider(c("task", "id"), names_from = "half", values_from = "index") |>
   group_by(task) |>
   summarise(
     r_split_half = cor(Even, Odd),
